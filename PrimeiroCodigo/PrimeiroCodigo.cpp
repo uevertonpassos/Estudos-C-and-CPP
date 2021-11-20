1b8dae4ed060bd077504d6e21d7c5c03e84fc7d8
Ueverton Passos <uevertonpassos96@gmail.com>
2021-11-20 13:01:41 -0300

    novo código

--- PrimeiroCodigo/PrimeiroCodigo.cpp
+++ PrimeiroCodigo/PrimeiroCodigo.cpp
@@ -1,7 +1,20 @@
-#include <iostream>
+#include <iostream> 
+//A diretiva de pré-processador #include
+//indica para incluir o arquivo de cabeçalho
 
-int main(){
+int main()
+//Função principal. Todo programa C++ começa com
+//a Função principal main()
+
+{ //chaves
+	//{indica o inicio de um novo bloco 
+	//cout comando de saida de dados
+	//similar ao print do python
+	//endl; indica quebra de linha do código
 	std::cout << "Primeiro Codigo" << std::endl;
+	system("color 2F");
 	system("PAUSE");
+	// System chama o shell do windows
 	return 0;
-}
+}//indica o final do bloco
+
